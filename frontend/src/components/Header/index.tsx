8227aacbcd0fe2e8e42e14bd6a3d05123b1e872e
Matheus Frej <matheusfrej@hotmail.com>
2023-07-19 12:53:10 -0300

    feat(Frej): Comecei a estrutura da home

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,7 +3,11 @@ import styles from './styles.module.css'
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-      <h2>Sigab</h2>
+      <div>
+        <h2>
+          Siga<span>b</span>
+        </h2>
+      </div>
     </header>
   )
 }
