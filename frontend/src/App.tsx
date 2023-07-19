28dc61dc0500849fbc2c25bb03927bf0e1d5e63d
Matheus Frej <matheusfrej@hotmail.com>
2023-07-18 21:20:26 -0300

    fix(Frej): mudei o Header para a pasta components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
-import { Header } from './pages/Header'
+import { Header } from './components/Header'
 
 function App() {
   return (
