f93c3e3c98aec095677755234eb806776fa16126
Matheus Frej <matheusfrej@hotmail.com>
2023-07-22 02:44:54 -0300

    Grade de horário parametrizada

--- frontend/src/components/Horario/index.tsx
+++ frontend/src/components/Horario/index.tsx
@@ -5,7 +5,27 @@ import { useEffect, useState, useContext } from 'react'
 
 export function Horario() {
   const { showToast } = useContext(SigabContext)
-  const [horario, setHorario] = useState()
+  const [horario, setHorario] = useState<object>()
+
+  const matrizHorario = [
+    ['vazio', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+    ['7h', '', '', '', '', '', ''],
+    ['8h', '', '', '', '', '', ''],
+    ['9h', '', '', '', '', '', ''],
+    ['10h', '', '', '', '', '', ''],
+    ['11h', '', '', '', '', '', ''],
+    ['12h', '', '', '', '', '', ''],
+    ['13h', '', '', '', '', '', ''],
+    ['14h', '', '', '', '', '', ''],
+    ['15h', '', '', '', '', '', ''],
+    ['16h', '', '', '', '', '', ''],
+    ['17h', '', '', '', '', '', ''],
+    ['18h', '', '', '', '', '', ''],
+    ['19h', '', '', '', '', '', ''],
+    ['20h', '', '', '', '', '', ''],
+    ['21h', '', '', '', '', '', ''],
+    ['22h', '', '', '', '', '', ''],
+  ]
 
   const getHorario = async () => {
     const token = localStorage.getItem('token')
@@ -24,10 +44,90 @@ export function Horario() {
 
   useEffect(() => {
     getHorario()
+    setHorario({
+      seg: {
+        '8': 'Programação Concorrente Distribuída',
+        '9': 'Programação Concorrente Distribuída',
+        '10': 'Análise e Projetos de Sistemas',
+        '11': 'Análise e Projetos de Sistemas',
+      },
+      ter: {
+        '8': 'Sistemas de Informação',
+        '9': 'Sistemas de Informação',
+        '10': 'Análise e Projetos de Sistemas',
+        '11': 'Análise e Projetos de Sistemas',
+      },
+      qua: {
+        '10': 'Programação Concorrente Distribuída',
+        '11': 'Programação Concorrente Distribuída',
+        '8': 'Análise e Projetos de Sistemas',
+        '9': 'Análise e Projetos de Sistemas',
+      },
+      qui: {
+        '10': 'Sistemas de Informação',
+        '11': 'Sistemas de Informação',
+        '8': 'Análise e Projetos de Sistemas',
+        '9': 'Análise e Projetos de Sistemas',
+      },
+      sex: {},
+      sab: {},
+    })
   }, [])
 
   return (
     <div className={styles.horarioContainer}>
+      <table>
+        {matrizHorario.map((horario, idx) => {
+          if (idx === 0) {
+            return (
+              <tr key={idx}>
+                {horario.map((value, idx) => {
+                  return (
+                    <>
+                      {value !== 'vazio' && (
+                        <th key={idx}>
+                          {value === 'seg'
+                            ? 'Segunda'
+                            : value === 'ter'
+                            ? 'Terça'
+                            : value === 'qua'
+                            ? 'Quarta'
+                            : value === 'qui'
+                            ? 'Quinta'
+                            : value === 'sex'
+                            ? 'Sexta'
+                            : 'Sábado'}
+                        </th>
+                      )}
+                      {value === 'vazio' && <th key={idx}></th>}
+                    </>
+                  )
+                })}
+              </tr>
+            )
+          }
+          return (
+            <tr key={idx}>
+              {horario.map((value, idx) => {
+                return (
+                  <>
+                    {value === '' && (
+                      <td key={idx}>
+                        <div className={styles.horarioCard}>
+                          <strong>Programação concorrente e distribuída</strong>
+                          <span>horario tal</span>
+                        </div>
+                      </td>
+                    )}
+                    {value !== '' && <td key={value + idx}>{value}</td>}
+                  </>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </table>
+      {/* 
       <table>
         <tr>
           <th></th>
@@ -230,7 +330,7 @@ export function Horario() {
         <tr>
           <td>22h</td>
         </tr>
-      </table>
+      </table> */}
     </div>
   )
 }
--- frontend/src/services/horarioService.ts
+++ frontend/src/services/horarioService.ts
@@ -2,8 +2,11 @@ import api from '../libs/api'
 
 export const getHorarioRequest = async (token: string) => {
   try {
+    const data = { token }
+    const info = JSON.stringify(data)
     const response = await api.get('/ver-horario', {
-      data: { token },
+      headers: { 'Content-Type': 'application/json' },
+      data: info,
     })
 
     return response.data
