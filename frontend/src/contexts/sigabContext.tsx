2e4594d483b9318ccf328941837cdd101a9b88a9
Matheus Frej <matheusfrej@hotmail.com>
2023-07-22 13:18:27 -0300

    Desloga o usuário quando o token expira

--- frontend/src/contexts/sigabContext.tsx
+++ frontend/src/contexts/sigabContext.tsx
@@ -29,17 +29,47 @@ export function SigabContextProvider({ children }: SigabContextProviderProps) {
   const login = async (email: string, senha: string) => {
     const response = await loginRequest(email, senha)
     const token = response.idToken
+    const expireDate = response.expiresIn
+    // console.log(expireDate)
+    const dataAtual = new Date()
+    const milissegundosAtuais = dataAtual.getTime()
+    const milissegundosMaisUmaHora = milissegundosAtuais + 10 * 1000
+    const novaData = new Date(milissegundosMaisUmaHora)
+
+    // console.log(novaData)
     setUserInfo(response.user)
     if (token === -1) {
       return false
     }
     localStorage.setItem('token', token.toString())
     localStorage.setItem('userInfo', JSON.stringify(response.user))
+    localStorage.setItem('tokenExpireDate', JSON.stringify(novaData))
+    return true
+  }
+
+  const didTokenExpire = () => {
+    if (localStorage.getItem('tokenExpireDate') !== null) {
+      const dataAtual = new Date().getTime()
+      let dataExpira = localStorage.getItem('tokenExpireDate')
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      dataExpira = JSON.parse(dataExpira!)
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const dataExpiraDate = new Date(dataExpira!).getTime()
+      if (dataAtual > dataExpiraDate) {
+        return true
+      }
+      return false
+    }
     return true
   }
 
   const isLogged = () => {
     if (localStorage.getItem('token') !== null) {
+      const tokenExpire = didTokenExpire()
+      if (tokenExpire) {
+        localStorage.clear()
+        return false
+      }
       const user = localStorage.getItem('userInfo')
       if (user !== null) {
         setUserInfo(JSON.parse(user))
